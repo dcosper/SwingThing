fd271c9477dd112832bac44a4ca62298bbab5a82
Dylan Cosper <dcosper1@stu.parkland.edu>
2023-05-04 09:12:26 -0500

    Unlimit the framerate

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import com.formdev.flatlaf.FlatDarkLaf
+import java.awt.Font
 import java.awt.KeyboardFocusManager
 import java.awt.Point
 import java.awt.Rectangle
@@ -24,7 +25,11 @@ data class Vec2(var x: Double = 0.0, var y: Double = 0.0) {
     }
 }
 
-class Object(text: String, private var pos: Vec2 = Vec2(), private var size: Vec2 = Vec2()) {
+interface GameObject {
+    fun update(dt: Double, posOffset: Vec2)
+}
+
+class Object(text: String, private var pos: Vec2 = Vec2(), private var size: Vec2 = Vec2()) : GameObject {
     private val button = JButton(text)
 
     init {
@@ -39,8 +44,8 @@ class Object(text: String, private var pos: Vec2 = Vec2(), private var size: Vec
         pos += by
     }
 
-    fun update(offset: Vec2) {
-        button.location = Point(pos.x.toInt() + offset.x.toInt(), pos.y.toInt() + offset.y.toInt())
+    override fun update(dt: Double, posOffset: Vec2) {
+        button.location = Point(pos.x.toInt() + posOffset.x.toInt(), pos.y.toInt() + posOffset.y.toInt())
     }
 
     fun collidingWith(other: Object): Boolean {
@@ -87,13 +92,13 @@ class World(private var window: JFrame, var objects: Array<Object>, private var 
         this.objectIndex = objectIndex
     }
 
-    fun update() {
+    fun update(dt: Double) {
         val focusedObject = this.objects[this.objectIndex]
         this.camera = focusedObject.pos() * -1.0
         this.camera -= focusedObject.size()/2.0
         this.camera += Vec2(window.width/2.0, window.height/2.0)
         for (obj in this.objects) {
-            obj.update(this.camera)
+            obj.update(dt, this.camera)
         }
     }
 }
@@ -105,16 +110,6 @@ fun keyPressed(event: KeyEvent): Boolean {
         KeyEvent.KEY_TYPED -> return true
     }
     return false
-}
-
-fun applyFriction(xVel: Double, grounded: Boolean): Double {
-    val friction = if (grounded) {
-        2.0
-    } else {
-        0.5
-    }
-
-    return max(abs(xVel) - friction, 0.0) * sign(xVel)
 }
 
 enum class Side {
@@ -144,6 +139,27 @@ fun newCollision(a: Object, b: Object): Side {
         rCollision -> Side.Right
         else -> {Side.Top}
     }
+}
+
+fun dtToFps(dt: Double): Double {
+    return 1.0/dt
+}
+
+// Pixels/Second
+const val SPEED = 600.0
+const val JUMP_SPEED = -1200.0
+const val GRAVITY = 3600.0
+const val GROUND_FRICTION = 7200.0
+const val AIR_FRICTION = 1800.0
+
+fun applyFriction(dt: Double, xVel: Double, grounded: Boolean): Double {
+    val friction = if (grounded) {
+        GROUND_FRICTION
+    } else {
+        AIR_FRICTION
+    } * dt
+
+    return max(abs(xVel) - friction, 0.0) * sign(xVel)
 }
 
 fun main() {
@@ -193,6 +209,11 @@ fun main() {
         val world = World(window, arrayOf(player, realGround, ground2, a, b, ceiling))
         world.setCameraFocus(0)
 
+        val fpsDisplay = JLabel("999 FPS")
+        fpsDisplay.setBounds(0, 50, 200, 20)
+        fpsDisplay.font = Font(fpsDisplay.font.name, fpsDisplay.font.style, 20)
+        window.add(fpsDisplay)
+
         window.isVisible = true
 
         var windowClosing = false
@@ -203,21 +224,36 @@ fun main() {
         var leftPressed = false
         var rightPressed = false
         var upPressed = false
+        val fpsHistory = Array(3000) {0}
+        var frameCount = 0L
         val vel = Vec2()
+        var lastFrameEnd = System.nanoTime()
         val worker = object : SwingWorker<Int, Int>() {
             override fun doInBackground(): Int {
                 while (true) {
-                    Thread.sleep(16)
+                    val thisFrameStart = System.nanoTime()
+                    val dt = (thisFrameStart - lastFrameEnd).toDouble() / 1_000_000_000.0
+                    lastFrameEnd = System.nanoTime()
 
-                    //val grounded = player.collidingWith(realGround) && newCollision(player, realGround) == Side.Top
+                    if (dt > 5)
+                        continue
+
+                    val fps = dtToFps(dt).toInt()
+                    val fpsHistoryIndex = (frameCount % 3000).toInt()
+                    if (fpsHistoryIndex == 0) {
+                        val fpsAverage = fpsHistory.average().toInt()
+                        fpsDisplay.text = "$fpsAverage FPS"
+                    }
+                    fpsHistory[fpsHistoryIndex] = fps
+
 
                     if (leftPressed) {
-                        vel.x = -10.0
+                        vel.x = -SPEED
                     } else if (rightPressed) {
-                        vel.x = 10.0
+                        vel.x = SPEED
                     }
 
-                    player.move(vel)
+                    player.move(vel * dt)
 
                     var grounded = false
                     for (obstacle in world.objects) {
@@ -254,22 +290,18 @@ fun main() {
                         }
                     }
 
-                    vel.x = applyFriction(vel.x, grounded)
+                    vel.x = applyFriction(dt, vel.x, grounded)
 
                     if (grounded) {
-                        vel.y = if (upPressed) {-20.0} else {0.0}
+                        vel.y = if (upPressed) {JUMP_SPEED} else {0.0}
                     } else {
-                        vel.y += 1.0
+                        vel.y += GRAVITY * dt
                     }
 
-                    world.update()
+                    world.update(dt)
                     window.repaint()
 
-//                    val distancePastBottom = (199 + player.pos.y) - realGround.pos.y
-//
-//                    if (distancePastBottom > 0) {
-//                        player.move(Vec2(0.0, -distancePastBottom))
-//                    }
+                    frameCount += 1
 
                     if (windowClosing) {
                         break
